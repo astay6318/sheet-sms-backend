d12320a74af4ddf91baca31d2f516596d44721e6
akshat-22 <aanshujaiswal135@gmail.com>
2023-11-21 03:11:19 +0530

    added comments

--- sheets/models.py
+++ sheets/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+#create the tables for form, questions,answers and response
 class Form(models.Model):
     form_id = models.AutoField(primary_key=True)
     form_name = models.CharField(max_length=255)
 
+#each question is linked to a form 
 class Question(models.Model):
     question_id = models.AutoField(primary_key=True)
     form = models.ForeignKey(Form,on_delete=models.CASCADE)
@@ -16,12 +18,14 @@ class Question(models.Model):
     )
     question_type = models.CharField(max_length=20, choices = question_type_choices,default=SINGLE_CHOICE)
 
+#answer is linked to the question and form using foreign key
 class Answer(models.Model):
     answer_id = models.AutoField(primary_key=True)
     question = models.ForeignKey(Question,on_delete=models.CASCADE)
     answer_text = models.CharField(max_length=255)
     is_correct = models.BooleanField(default=False)
 
+#response is connected to the form and question using foreign key
 class Response(models.Model):
     user_id = models.IntegerField()
     form = models.ForeignKey(Form,on_delete=models.CASCADE)
--- sheets/signals.py
+++ sheets/signals.py
@@ -1,4 +1,5 @@
-# code
+#signals used to perform particular action on the modification/creation of a particular entry in the Database
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Response
@@ -8,46 +9,44 @@ from dotenv import load_dotenv
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+#load the env variables
 load_dotenv()
 
 sheet_id = os.environ.get("sheet_id")
 api_key = os.environ.get("api_key")
 
-# define the scope
+# define the scope,i.e., the level of access the application is requesting. Here google sheetsdata and google drive data is required
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
+
+#Send the message and update to google sheets after a response has been saved
 @receiver(post_save,sender=Response) 
 def create_profile( sender,instance, created, **kwargs):
 	if created:
 		account_sid = os.environ.get("twilio_account_sid")
 		auth_token = os.environ.get("twilio_auth_token")
 		client = Client(account_sid, auth_token)
-		available_phone_number_country = client.available_phone_numbers('US').fetch()
-		print(instance.user_id)
-		print(instance.question.question_text)
-		print(instance.answers)
+
+		#define the body of the message to be sent to the client (dictionary)
 		body={
 			"user_id": instance.user_id,
 			"question":instance.question.question_text,
 			"answer":instance.answers
 		}
+
 		# Update the path to the JSON file
 		json_file_path = 'sheets/abc.json'
-
+		#first update in the sheets
 		try:
-			# Add exception handling for file-related errors
+			# get the credentials of the service account using the oauth2client library and authorize the credentials
 			creds = ServiceAccountCredentials.from_json_keyfile_name(json_file_path, scope)
 			gspread_client = gspread.authorize(creds)
-			# print("Akshat" * 100)
-			# get the instance of the Spreadsheet
-			sheet = gspread_client.open('commentary data')
 
-			# get the first sheet of the Spreadsheet
+			# get the instance of the Spreadsheet with the sheet_id
+			sheet = gspread_client.open_by_key(sheet_id)
+
+			# get the first sheet of the Spreadsheet (assuming only 1 form)
 			sheet_instance = sheet.get_worksheet(0)
-			# print(sheet_instance/)
-			# get the total number of columns
-			sheet_instance.col_count
-			## >> 26
 
 			# Find the first empty row
 			next_row = len(sheet_instance.get_all_values()) + 1
@@ -63,6 +62,7 @@ def create_profile( sender,instance, created, **kwargs):
 		except Exception as e:
 			print(f"Error loading credentials from {json_file_path}: {e}")
 			return
+		#send an sms to the client
 		try:
 			message = client.messages.create(
 			from_='+19528563009',
